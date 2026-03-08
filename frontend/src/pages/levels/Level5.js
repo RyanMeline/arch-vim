93b141a51bf5c9301f5ba9534c4b07d69cc7ba17
Ryan Meline <ryan.c.meline@gmail.com>
2026-03-08 16:51:18 -0700

    Level 5 complete

--- frontend/src/pages/levels/Level5.js
+++ frontend/src/pages/levels/Level5.js
@@ -1,13 +1,128 @@
 import { Link } from "react-router-dom";
 import VimEditor from "../../editor/vimEditor"
+import Editor from "@monaco-editor/react";
+import { useRef, useState } from "react";
+import { initVimMode, VimMode } from "monaco-vim";
 
-export default function Level1() {
-    return (
+//rebind :w to the check function to check whats written
+
+export default function Level5() {
+  const [passed, setPassed] = useState(false);
+
+  VimMode.Vim.defineEx("write", "w", function(cm, input) {
+    checkAnswer();
+  });
+
+  const editorRef = useRef(null);
+    const vimModeRef = useRef(null);
+  
+    function handleMount(editor, monaco) {	
+      editorRef.current = editor;
+      const editorDom = editor.getDomNode();
+      editorDom.style.position = "relative";
+      
+      //Vim current mode at bottom
+      const statusNode = document.createElement("div");
+      statusNode.style.position = "absolute";
+      statusNode.style.bottom = "0";
+      statusNode.style.right = "50px";
+      statusNode.style.background = "#1e1e1e";
+      statusNode.padding = "4px 8px";
+      statusNode.style.fontSize = "12px";
+    
+      editor.getDomNode().appendChild(statusNode);
+      vimModeRef.current = initVimMode(editor, statusNode);
+  
+      //Cursor line info at bottom
+      const cursorPosNode = document.createElement("div");
+      cursorPosNode.style.position = "absolute";
+      cursorPosNode.style.bottom = "0";
+      cursorPosNode.style.left = "35px";
+      cursorPosNode.style.background = "#1e1e1e";
+      cursorPosNode.padding = "4px 8px";
+      cursorPosNode.style.fontSize = "12px";
+  
+      editor.getDomNode().appendChild(cursorPosNode);
+      
+      editor.onDidChangeCursorSelection(e => {
+        console.log("Cursor Info: ", e);
+        cursorPosNode.innerText = `Ln ${e.selection.positionLineNumber}, Col ${e.selection.positionColumn}`;
+      });
+    
+      //Key logger (use for checking for certain key presses)
+      editor.onKeyDown((e) => {
+        console.log("Key pressed: ", e.browserEvent.key);
+      });
+    }
+  
+    //Editor saves to memory, checks against that
+    function checkAnswer() {
+      const expectedSolution = 
+`#include <stdio.h>
+  
+void main() {
+  printf("Goodbye World");
+  return 0; 
+}`;
+      const userCode = editorRef.current.getValue();
+      console.log("User code: ", userCode);
+  
+      if(userCode.trim() === expectedSolution.trim()) {
+        setPassed(true);
+        //whatever else for correct
+      } else {
+        console.log("wrong");
+      }
+    }
+    //Build text box and check button
+    return(
+      <>
       <div>
         <h1>Challenge Level!</h1>
         <h3>Combine all the skills you've learned to complete the objective!</h3>
-        <p>Objective: Change Hello World to Goodbye World, and save.</p>
-        <VimEditor />
-      </div>  
-    );
+        <p>Objective: Change 'Hello World' to 'Goodbye World', and save.</p>
+      </div>
+      {passed && (
+            <div style={{
+                marginTop: "20px",
+                padding: "10px",
+                background: "#1e1e1e",
+                border: "1px solid #4caf50",
+                borderRadius: "5px"
+            }}>
+                <h3 style={{ color: "#4caf50" }}>You passed!</h3>
+                <p style = {{ color: "white" }}>
+                    Thats if for now, thanks for playing!
+                </p>
+                <p style = {{ color: "white" }}>
+                    Go back home:
+                    <Link to="/" style= {{ marginLeft: "8px"}}>
+                        Home
+                    </Link>
+                </p>
+            </div>
+    )}<br></br>
+      <Editor
+      height = "500px"
+      width = "1000px"
+      theme = "vs-dark"
+      defaultLanguage="c" //This is for highlighting
+      defaultValue=
+  { //Code that appears on screen
+`#include <stdio.h>
+  
+void main() {
+  printf("Hello World");
+  return 0; 
+}`
+}
+      options = {{
+        minimap: { enabled: false }
+      }}
+      onMount={handleMount}
+      />
+  
+      <button onClick={checkAnswer}>Check</button>
+      </>
+          );  
 }
